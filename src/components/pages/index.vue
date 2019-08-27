fbeb3f7a82ee18ee7caf84ba5c1bc5aecd257c6e
dingsan <1398179322@qq.com>
2019-08-27 10:58:36 +0800

    初始化仓库

---
+++ src/components/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <el-container>
+        <el-header>Header</el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<style>
+    .el-header {
+        background-color: #ccc;
+    }
+    .el-aside {
+        background-color: pink;
+    }
+    .el-main {
+        background-color: #000;
+    }
+</style>
